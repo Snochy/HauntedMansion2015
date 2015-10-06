0e1f1970a888998cbe0b4b338333ec9801ffe77c
Zachary Thorpe <zachauna@gmail.com>
2015-10-06 01:08:01 -0600

    -Light flicker fixed

---
+++ RavenHill/Assets/Scripts/LightClicker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightClicker : MonoBehaviour {
+
+    public float timeSpentOn = 2;
+
+    private float timer;
+	// Update is called once per frame
+	void Update () 
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            GetComponent<Light>().enabled = false;
+        }
+
+        if(GetComponent<Light>().enabled == false)
+        {
+	        float ranInt = Random.Range(0,90);
+            if(ranInt == 0)
+            {
+                timer = Random.Range(0, 1.1f) + timeSpentOn;
+                GetComponent<Light>().enabled = true;
+            }
+        }
+	}
+}
