3f1760087961fe6086d76d7ed53244ebd3562012
505 Computer A <zachauna@gmail.com>
2015-10-26 16:51:26 -0600

    -Substance material fixed to include changed variables

---
+++ RavenHill/Assets/Scripts/LightSnapper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSnapper : MonoBehaviour {
+
+	public GameObject target;
+
+	void Update () 
+	{
+		if (target != null)
+			transform.position = target.transform.position;
+	
+	}
+}
